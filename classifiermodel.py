e7ff9e41a969715e3b2af96e3b797532fe230d71
zhoujiaxing <zhoujiaxing@hinterstellar.com>
2016-04-07 18:18:14 +0800

    edit on xing-pc

--- classifiermodel.py
+++ classifiermodel.py
@@ -9,7 +9,7 @@ cates = ["Auto","Business","Cricket","Entertainment/Bollywood","Health","Lifesty
 class ClassifierModel(object):
 	def __init__(self):
 		self.dirctory = {}
-		self.vectorizer = TfidfVectorizer()
+		self.vectorizer = TfidfVectorizer(vocabulary=getVocabulary())
 		self.classifiers = {}
 		self.getDirctory()
 		self.trainVectorizer()
@@ -28,28 +28,28 @@ class ClassifierModel(object):
 				else:
 					self.dirctory.update({category:[text]})
 			except:
-				"No expect...."
+				"No expect....i"
 		print "Dirctory Categoey number is %d..."%len(self.dirctory.keys())
 	def trainVectorizer(self):
 		corpus = []
 		for category in cates:
-			corpus.extend(self.dirctory[category][0:5000])
+			corpus.extend(self.dirctory[category])
 		self.vectorizer.fit_transform(corpus)
 		print "train vector number is %d...."%len(corpus)
 	def trainClassifiers(self):
-		number = 300
+		number = 1000
 		for category in cates:
 			classifier = LogisticRegression()
 			train_x = []
 			train_y = []
 			for cate in cates:
 				if cate == category:
-					texts = self.dirctory[cate][0:number*10]
+					texts = self.dirctory[cate][5000:5000+number*10]
 					for text in texts:
 						train_x.append(self.getFeature(text))
 						train_y.append(1)
 				else:
-					texts = self.dirctory[cate][0:number]
+					texts = self.dirctory[cate][5000:5000+number]
 					for text in texts:
 						train_x.append(self.getFeature(text))
 						train_y.append(0)
@@ -60,13 +60,36 @@ class ClassifierModel(object):
 	def getFeature(self,text):
 		return self.vectorizer.transform([text]).toarray()[0]
 	def getCategorys(self,text):
+		deci_param = {}
+		deci_param["Cricket"] = 0.7
+		deci_param["Lifestyle"] = 0.7
+		deci_param["Business"] = 0.7
+		deci_param["Auto"] = 0.7
+		deci_param["National"] = 0.7
+		deci_param["Entertainment/Bollywood"] = 0.7
+		deci_param["Sports"] = 0.7
+		deci_param["Health"] = 0.7
+		deci_param["World"] = 0.7
+		deci_param["Politics"] = 0.7
+		deci_param["Technology"] = 0.7
+
 		feature = self.getFeature(text)
 		categorys = []
 		for cate in cates:
 			classifier = self.classifiers[cate]
-			ans = classifier.predict(feature)[0]
-			if ans == 1:
+			ans = classifier.decision_function(feature)[0]
+			if ans > deci_param[cate]:
 				categorys.append(cate)
 		if categorys == []:
 			return ['Other']
 		return categorys
+def getVocabulary():
+	vocabulary = {}
+	file1 = open("tmp.txt","r")
+	count = 0
+	lines = file1.readlines()
+	for line in lines:
+		vocabulary.update({line.strip():count})
+		count = count + 1
+	file1.close()
+	return vocabulary
--- test.py
+++ test.py
@@ -8,18 +8,23 @@ if __name__ == "__main__":
 	print "vecabulary number is %d ...."%len(words)
 	file1 = open("vecabulary.txt","w")
 	for word in words:
-		file1.write(word.encode('utf8')+"\n")
+		file1.write(word+"\n")
 	file1.close()
 	#test
+	ret = {}
+	for cate in cates:
+		ret[cate] = 0
+	ret["Other"] = 0
 	dirc = model.dirctory
 	for cate in cates:
-		texts = dirc[cate][5000:6000]
-		fileopen = open(cate[0:13]+".txt","w")
+		texts = dirc[cate][8000:9000]
+		fileopen = open('./test5/'+cate[0:13]+".txt","w")
 		fileopen.write(str(len(texts))+'\n')
 		count = 0
 		for text in texts:
 			categorys = model.getCategorys(text)
 			for category in categorys:
+				ret[category] = ret[category] + 1
 				fileopen.write(category+' ')
 				if cate ==  category:
 					count = count + 1
@@ -27,4 +32,7 @@ if __name__ == "__main__":
 		fileopen.write(str(count))
 		fileopen.write(str(float(count)/float(len(texts))*100)+'\%')
 		fileopen.close()
+		file1 = open("./test5/"+"ret.txt","w")
+		file1.write(str(ret))
+		file1.close()
 		print "%s test is over...."%cate
